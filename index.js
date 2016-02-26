e2af097f18c1e4cefeef5ba29d3dffdb5b447049
Carl Kashnier <Tetum007@gmail.com>
2016-02-26 17:16:00 -0500

    Updaing with the basic server script

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+//dotenv will load the .env file for the local environment
+//This keeps my API and Mongo_URI String out of view for streaming
+ 
+var dotenv = require('dotenv').config();
 var http = require('http')
 var url = require('url')
 var mongo = require('mongodb')
+var sanitize = require('sanatize-caja')
+
+var server = http.createServer(function (req, res) {
+
+
+});
+server.listen(process.env.PORT || 8888);
