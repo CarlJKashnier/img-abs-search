7d8a6503e99efb5d67d8b588a24e7079a0416c98
Carl Kashnier <Tetum007@gmail.com>
2016-03-01 11:04:37 -0500

    Works 100% of the challange, now to put on heroku

--- index.js
+++ index.js
@@ -27,27 +27,39 @@ if(parsedURL.path.substring(0, 9) == '\/api\/img\/') {
     //check if offset is good if not then return 0 for query
   if(parsedURL.query.substring(0,7) === "offset=" && !isNaN(parsedURL.path.substring(parsedURL.pathname.length + 8))) {
   var offset = parsedURL.path.substring(parsedURL.pathname.length + 8);
-  console.log(searchTerm +' '+offset + " Second")
-      bingSearch(searchTerm, offset)
+  bingSearch(searchTerm, offset)
+  return
   //Add Call to Search function
-} else {
-  if(parsedURL.path.length == parsedURL.pathname.length){
-    var offset = 0;
-    console.log(searchTerm +' '+offset + " Third")
-    // Add call to Search function
-    bingSearch()
-  } else {
+}  else {
     //send error
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify({"error": "Malformed offset"}));
   res.end();
   return;
-}}}}
+}}};
 
-return
-//Function is in server so can use the REQ variable
+
+if(parsedURL.path.substring(0, 7) == '\/recent') {
+
+  mongo.connect(process.env.MONGOLAB_URI,function(err,db){
+
+   db.collection('searchhx').count(function(e, count){
+  db.collection('searchhx').find({localID: {$gt: count-10}},{searchterm: 1, when: 1, _id: 0}).toArray(function(err, stuff){
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write(JSON.stringify(stuff));
+  res.end();
+})
+})
+})
+}
+
+
+
+
+
+//Function is in server so can use the res variable, good times.
 function bingSearch(searchTerm, offsetSearch){
-  bing.images(searchTerm, function(err, resp, body){
+  bing.images(searchTerm, {skip:offsetSearch}, function(err, resp, body){
   var toClientOut = []
   for(var i=0; i<10;i++){
     var toPushObj = {"url": body.d.results[0].MediaUrl, "snippet": body.d.results[0].Title, "thumbnail": body.d.results[0].Thumbnail.MediaUrl, "context": body.d.results[0].SourceUrl}
@@ -56,9 +68,24 @@ function bingSearch(searchTerm, offsetSearch){
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(toClientOut));
   res.end();
+
+  mongo.connect(process.env.MONGOLAB_URI,function(err,db){
+//get number of records & add to DB
+    var currentRecord = db.collection('searchhx').count(function(e, count){
+      var host = req.headers['x-forwarded-for'] ||
+       req.connection.remoteAddress ||
+       req.socket.remoteAddress ||
+       req.connection.socket.remoteAddress;
+       db.collection('searchhx').insert({ip: host, searchterm: searchTerm, when: Date(), localID: count + 1})
+    })
+
+      db.close
+});
+
+    });
   return
-  })
-}
+};
+
 
 });
 server.listen(process.env.PORT || 8888);
